2b0ada435ad91135fdcc6b2fae2b65b25cbc06d7
Yan Basílio <69776644+YanFellippe@users.noreply.github.com>
2024-10-16 20:39:59 -0300

    Add files via upload

---
+++ backend/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+
+class TaskBase(BaseModel):
+    title: str
+    description: str = None
+
+class TaskCreate(TaskBase):
+    pass
+
+class TaskUpdate(TaskBase):
+    completed: bool
+
+class Task(TaskBase):
+    id: int
+    completed: bool
+
+    class Config:
+        orm_mode = True
